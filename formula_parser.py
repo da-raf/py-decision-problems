488c635adbd147aa1199288dbac0120c00062ede
Raphael Dümig <duemig@posteo.de>
2024-12-23 15:02:04 +0100

    parser: support for implications and equivalences

--- formula_parser.py
+++ formula_parser.py
@@ -19,17 +19,34 @@ def or_conv(toks):
 def and_conv(toks):
     return sx.And(list(toks)) if len(toks) > 1 else toks[0]
 
+def impl_conv(toks):
+    res = toks[0]
+    for tok in toks[1:]:
+        res = sx.Implication(res, tok)
+    return res
+
+def equiv_conv(toks):
+    res = toks[0]
+    for tok in toks[1:]:
+        res = sx.Equivalence(res, tok)
+    return res
+
 def gen_conv(toks):
     return toks[0]
 
+def to_operator(lits):
+    return functools.reduce(pp.ParserElement.__or__, map(pp.Literal, lits))
+
 
 class FormulaParser:
-    def __init__(self, neg_ops=("¬", "-"), or_ops=("+", "∨"), and_ops=("*", "∧"), open_bracket=("(",), close_bracket=(")",)):
-        self.and_op = functools.reduce(pp.ParserElement.__or__, map(pp.Literal, and_ops))
-        self.or_op = functools.reduce(pp.ParserElement.__or__, map(pp.Literal, or_ops))
-        self.neg_op = functools.reduce(pp.ParserElement.__or__, map(pp.Literal, neg_ops))
-        self.bracket_open = functools.reduce(pp.ParserElement.__or__, map(pp.Literal, open_bracket))
-        self.bracket_close = functools.reduce(pp.ParserElement.__or__, map(pp.Literal, close_bracket))
+    def __init__(self, neg_ops=("¬", "-"), or_ops=("+", "∨"), and_ops=("*", "∧"), impl_ops=("=>",), equiv_ops=("<=>",), open_bracket=("(",), close_bracket=(")",)):
+        self.and_op = to_operator(and_ops)
+        self.or_op = to_operator(or_ops)
+        self.neg_op = to_operator(neg_ops)
+        self.impl_op = to_operator(impl_ops)
+        self.equiv_op = to_operator(equiv_ops)
+        self.bracket_open = to_operator(open_bracket)
+        self.bracket_close = to_operator(close_bracket)
         self.literal_chars = pp.alphas
 
         self.formula = pp.Forward()
@@ -39,7 +56,9 @@ class FormulaParser:
         self.neg_formula = (pp.ZeroOrMore(self.neg_op) + self.base_formula).set_parse_action(neg_conv)
         self.and_formula = pp.DelimitedList(self.neg_formula, self.and_op).set_parse_action(and_conv)
         self.or_formula = pp.DelimitedList(self.and_formula, self.or_op).set_parse_action(or_conv)
-        self.formula << self.or_formula
+        self.impl_formula = pp.DelimitedList(self.or_formula, self.impl_op).set_parse_action(impl_conv)
+        self.equiv_formula = pp.DelimitedList(self.impl_formula, self.equiv_op).set_parse_action(equiv_conv)
+        self.formula << self.equiv_formula
 
     def parse(self, f):
         return self.formula.parse_string(f)[0]
