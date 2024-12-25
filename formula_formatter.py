f7bb2e624a1066b3b90b1e47bac559997ff34130
Raphael Dümig <duemig@posteo.de>
2024-12-25 22:47:33 +0100

    improve formula formatting
    - clean up code
    - less unnecessary brackets
    - fix formatting of empty disjunctions and conjunctions

--- formula_formatter.py
+++ formula_formatter.py
@@ -7,81 +7,69 @@ provides printing functionality for formulas
 from syntax import *
 import enum
 
+class OperatorSet:
+    def __init__(self, true_sym, false_sym, neg_op, conj_op, disj_op, impl_op, equv_op):
+        self.true = true_sym
+        self.false = false_sym
+        self.negation = neg_op
+        self.conjunction = conj_op
+        self.disjunction = disj_op
+        self.implication = impl_op
+        self.equivalence = equv_op
+
+unicode_ops = OperatorSet('⊤', '⊥', '¬', '∧', '∨', '=>', '<=>')
+ascii_ops = OperatorSet('T', 'F', '~', '*', '+', '=>', '<=>')
+
 class FormulaFormatter:
-    """
-    formats formula with scientific symbols (requires unicode) ¬/∨/∧/=>/<=>
-
-    >>> FormulaFormatter().format(formula)
-    'x_1=>¬((¬a∨b)∧(a∨¬b))'
-    """
+    def __init__(self, operators=unicode_ops):
+        self.operators = operators
 
     def format(self, formula, top_level=True):
-        if type(formula) == Literal:
-            return ('¬' if formula.negated else '') + formula.name
-        elif type(formula) == And:
-            content = '∧'.join(self.format(child, False) for child in formula.children)
-            return content if top_level else '(%s)' % content
-        elif type(formula) == Or:
-            content = '∨'.join(self.format(child, False) for child in formula.children)
-            return content if top_level else '(%s)' % content
-        elif type(formula) == Negation:
-            return '¬' + self.format(formula.child, False)
-        elif type(formula) == Implication:
-            content = self.format(formula.lhs, False) + '=>' + self.format(formula.rhs, False)
+        if isinstance(formula, bool):
+            return (self.operators.true if formula else self.operators.false)
+        elif isinstance(formula, Literal):
+            return (self.operators.negation if formula.negated else '') + formula.name
+        elif isinstance(formula, And):
+            if len(formula.children) == 0:
+                content = self.operators.true
+            else:
+                content = self.operators.conjunction.join(self.format(child, False) for child in formula.children)
+            return content if top_level or len(formula.children) <= 1 else '(%s)' % content
+        elif isinstance(formula, Or):
+            if len(formula.children) == 0:
+                content = self.operators.false
+            else:
+                content = self.operators.disjunction.join(self.format(child, False) for child in formula.children)
+            return content if top_level or len(formula.children) <= 1 else '(%s)' % content
+        elif isinstance(formula, Negation):
+            return self.operators.negation + self.format(formula.child, False)
+        elif isinstance(formula, Implication):
+            content = self.format(formula.lhs, False) + self.operators.implication + self.format(formula.rhs, False)
             return content if top_level else '(%s)' % content 
-        elif type(formula) == Equivalence:
-            content = self.format(formula.lhs, False) + '<=>' + self.format(formula.rhs, False)
+        elif isinstance(formula, Equivalence):
+            content = self.format(formula.lhs, False) + self.operators.equivalence + self.format(formula.rhs, False)
             return content if top_level else '(%s)' % content
         else:
             raise SyntaxError("unknown formula type")
 
-class AsciiFormulaFormatter:
-    """
-    formats formula using ascii characters ~/+/*/=>/<=>
 
-    >>> AsciiFormulaFormatter().format(formula)
-    'x_1=>(~(((~a)+b)*(a+(~b))))'
-    """
-
-    def format(self, formula, top_level=True):
-        if type(formula) == Literal:
-            content = ('~' if formula.negated else '') + formula.name
-            return content if top_level or not formula.negated else '(%s)' % content
-        elif type(formula) == And:
-            content = '*'.join(self.format(child, False) for child in formula.children)
-            return content if top_level else '(%s)' % content
-        elif type(formula) == Or:
-            content = '+'.join(self.format(child, False) for child in formula.children)
-            return content if top_level else '(%s)' % content
-        elif type(formula) == Negation:
-            content = '~' + self.format(formula.child, False)
-            return content if top_level else '(%s)' % content
-        elif type(formula) == Implication:
-            content = self.format(formula.lhs, False) + '=>' + self.format(formula.rhs, False)
-            return content if top_level else '(%s)' % content 
-        elif type(formula) == Equivalence:
-            content = self.format(formula.lhs, False) + '<=>' + self.format(formula.rhs, False)
-            return content if top_level else '(%s)' % content
-        else:
-            raise SyntaxError("unknown formula type")
-    
 def get_formatter(unicode=False):
     """
     >>> import curses.ascii
-    >>> all(curses.ascii.isascii(c) for c in get_formatter(unicode=False).format(formula))
+    >>> all(curses.ascii.isascii(c) for c in get_formatter(unicode=False).format(all_ops))
     True
     """
 
     if unicode:
-        return FormulaFormatter()
+        return FormulaFormatter(unicode_ops)
     else:
-        return AsciiFormulaFormatter()
+        return FormulaFormatter(ascii_ops)
 
 if __name__ == '__main__':
     import doctest
 
     # formula that will be tested
-    formula = Implication(Literal('x_1'), Negation(And([Or([Literal('a', True), Literal('b')]), Or([Literal('a'), Literal('b', True)])])))
+    all_ops = Equivalence(Literal('a'), Implication(Literal('b'), And([Literal('c'), Or([Literal('d'), Negation(Literal('e'))])])))
 
     doctest.testmod()
 
